60f7db7989adc185ec9fb7e3bfa62490020b6dca
Chaman Vétéran <nathm.va@gmail.com>
2023-12-21 18:30:28 +0100

    Layout now taken from file

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Keyboard
+## GHC version used
+**ghc   9.8.1      base-4.19.0.0**
+
 ## To start the virtual keyboard using cabal
 ```
 cabal run keyboard
--- src/Main.hs
+++ src/Main.hs
@@ -1,71 +1,91 @@
 {-# LANGUAGE OverloadedStrings, OverloadedLabels, ImplicitParams #-}
 {-# LANGUAGE MonoLocalBinds #-}
 
+-- --------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+--
+--
+--  Virtual keyboard for linux.
+--
+-----------------------------------------------------------------------------
+
 -- Documentation :
 -- https://hackage.haskell.org/package/gi-gtk
 
 
+import Control.Monad (join)
 import Control.Monad.IO.Class (MonadIO)
 import Data.Text (Text, singleton)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, fromJust)
 import Data.Int (Int32)
+import Data.Aeson (Array, Value(Object, String, Array), decode)
+import Data.Vector (Vector)
+import Data.Aeson.KeyMap (Key)
+import qualified Data.Vector as V
+import qualified Data.Aeson.KeyMap as KM ((!?))
+import qualified Data.ByteString.Lazy as B (readFile)
 
+import GI.Gtk (Button, Box, IsWidget, Application)
 import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
 import Data.GI.Base (new, AttrOp((:=)), after, on)
 
-type CharLayout = [[Text]]
-type ButtonLayout = [[Gtk.Button]]
+type TextLayout = V.Vector (V.Vector Text)
+type ButtonLayout = V.Vector (V.Vector Gtk.Button)
 
-qwertyLayout :: CharLayout
-qwertyLayout = map (map singleton)
-    [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-     ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.']
-    ]
+-- | Extracts the text value out of a json
+getFromJSON :: Key -> Value -> Text
+getFromJSON key (Object o) = (\(String s) -> s) . fromJust $ o KM.!? key
 
-printLabel :: Gtk.Button -> IO ()
+qwertyLayout :: IO TextLayout
+qwertyLayout = do
+  layoutB <- B.readFile "layouts/qwerty.json"
+  let layoutArray = fromJust (decode layoutB :: Maybe Array)
+  let fromJSON = V.map (\(Array v) -> V.map (getFromJSON "label") v) layoutArray
+  return fromJSON
+
+printLabel :: Button -> IO ()
 printLabel b = do
     label <- Gtk.buttonGetLabel b
     print $ fromMaybe "Missing label" label
 
-listLines :: Int -> IO [Gtk.Box]
-listLines n = sequence $ replicate n $ new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+listLines :: Int -> IO (Vector Box)
+listLines n = sequence $ V.replicate n $ new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
 
-listButtons :: Int -> IO [Gtk.Button]
-listButtons n = sequence $ replicate n $ new Gtk.Button []
+listButtons :: Int -> IO (Vector Button)
+listButtons n = sequence $ V.replicate n $ new Gtk.Button []
 
-initButton :: Gtk.Button -> Text -> IO ()
+initButton :: Button -> Text -> IO ()
 initButton button label = do
     after button #clicked $ printLabel button
     Gtk.buttonSetLabel button label
 
-labelsToButtons :: [Text] -> [Gtk.Button] -> IO ()
-labelsToButtons labels buttons = sequence_ $ zipWith initButton buttons labels
+labelsToButtons :: Vector Text -> Vector Button -> IO ()
+labelsToButtons labels buttons = sequence_ $ V.zipWith initButton buttons labels
 
-appendTo :: Gtk.IsWidget w => Gtk.Box -> w -> IO ()
+appendTo :: IsWidget w => Box -> w -> IO ()
 appendTo = Gtk.boxAppend
 
-appendAllTo :: Gtk.IsWidget w => Gtk.Box -> [w] -> IO ()
-appendAllTo keyboard ws = sequence_ $ map (appendTo keyboard) ws
+appendAllTo :: IsWidget w => Box -> Vector w -> IO ()
+appendAllTo keyboard ws = sequence_ $ V.map (appendTo keyboard) ws
 
-applyLayout :: CharLayout -> IO ButtonLayout
-applyLayout = sequence . map (\l -> listButtons $ length l) 
+applyLayout :: TextLayout -> IO ButtonLayout
+applyLayout = sequence . V.map (\l -> listButtons $ V.length l) 
 
-activateApp :: Gtk.Application -> IO ()
+activateApp :: Application -> IO ()
 activateApp app = do
   keyboard <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
-  lines <- listLines (length qwertyLayout)
-  letters' <- applyLayout qwertyLayout
-  sequence_ $ map (uncurry labelsToButtons) $ zip qwertyLayout letters'
+  chosenLayout <- qwertyLayout
+  lines <- listLines (length chosenLayout)
+  letters <- applyLayout $ chosenLayout
+  sequence_ $ V.map (uncurry labelsToButtons) $ V.zip chosenLayout letters
 
   appendAllTo keyboard lines
-  sequence_ $ map (uncurry appendAllTo) $ zip lines letters'
+  sequence_ $ V.map (uncurry appendAllTo) $ V.zip lines letters
 
   w <- new Gtk.ApplicationWindow [ #application := app
                                  , #title := "Virtual Keyboard"
-                                 , #defaultHeight := 400
-                                 , #defaultWidth := 1000
                                  , #child := keyboard
                                  ]
   #show w
