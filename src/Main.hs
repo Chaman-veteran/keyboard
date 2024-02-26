b5967dc4da842a8b3e87aa93f372d71566cc6039
Chaman Vétéran <nathm.va@gmail.com>
2024-02-26 18:19:26 +0100

    Added keyboard's suggestion bar

--- src/Main.hs
+++ src/Main.hs
@@ -16,13 +16,13 @@
 
 import Control.Monad (join)
 import Control.Monad.IO.Class (MonadIO)
-import Data.Text (Text, singleton)
+import Data.Text (Text, singleton, pack)
 import Data.Maybe (fromMaybe, fromJust)
 import Data.Int (Int32)
 import Data.Aeson (Array, Value(Object, String, Array), decode)
 import Data.Vector (Vector)
 import Data.Aeson.KeyMap (Key)
-import Data.IORef (IORef, newIORef, readIORef, modifyIORef')
+import Data.IORef (IORef, newIORef, readIORef, modifyIORef', writeIORef)
 import qualified Data.Text as T (head)
 import qualified Data.Vector as V
 import qualified Data.Aeson.KeyMap as KM ((!?))
@@ -38,15 +38,26 @@ type ButtonLayout = V.Vector (V.Vector Gtk.Button)
 type Pipe = IORef String
 
 -- | Prints hitted button to stdout
-printLabel :: Pipe -> Button -> IO ()
-printLabel pipe b = do
+printLabel :: (Pipe, Button) -> Button -> IO ()
+printLabel (pipe, ks) b = do
     label <- Gtk.buttonGetLabel b
     case label of
       Nothing -> return ()
       Just c -> modifyIORef' pipe (\s -> T.head c : s)
-    s <- readIORef pipe
-    print s
+    refreshOutput pipe ks
     print $ fromMaybe "Missing label" label
+
+-- | Clears the pipe for keyboard's output
+clearPipe :: IORef String -> Button -> IO ()
+clearPipe pipe ks = do
+  writeIORef pipe ""
+  refreshOutput pipe ks
+
+-- | Refreshs the keyboard's suggestion output 
+refreshOutput :: IORef String -> Button -> IO ()
+refreshOutput pipe ks = do
+  pipeOutput <- readIORef pipe
+  Gtk.buttonSetLabel ks $ (pack . reverse) pipeOutput
 
 -- | Extracts the text value out of a json
 getFromJSON :: Key -> Value -> Text
@@ -69,13 +80,13 @@ listButtons :: Int -> IO (Vector Button)
 listButtons n = sequence $ V.replicate n $ new Gtk.Button []
 
 -- | Links a button to the associated label
-initButton :: Pipe -> Button -> Text -> IO ()
+initButton :: (Pipe, Button) -> Button -> Text -> IO ()
 initButton pipe b label = do
     after b #clicked $ printLabel pipe b
     Gtk.buttonSetLabel b label
 
 -- | Associates labels and buttons together
-labelsToButtons :: Pipe -> Vector Text -> Vector Button -> IO ()
+labelsToButtons :: (Pipe, Button) -> Vector Text -> Vector Button -> IO ()
 labelsToButtons pipe labels bs = sequence_ $ V.zipWith (initButton pipe) bs labels
 
 -- | Draws (appends) a list of widgets to the keyboard 
@@ -90,10 +101,15 @@ applyLayout = sequence . V.map (\l -> listButtons $ V.length l)
 activateApp :: Pipe -> Application -> IO ()
 activateApp pipe app = do
   keyboard <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+  
+  keyboardSuggestion <- new Gtk.Button []
+  after keyboardSuggestion #clicked $ clearPipe pipe keyboardSuggestion
+  drawOn keyboard $ V.singleton keyboardSuggestion
+
   chosenLayout <- qwertyLayout
   lines <- listRows (length chosenLayout)
   letters <- applyLayout $ chosenLayout
-  sequence_ $ V.map (uncurry $ labelsToButtons pipe) $ V.zip chosenLayout letters
+  sequence_ $ V.map (uncurry $ labelsToButtons (pipe, keyboardSuggestion)) $ V.zip chosenLayout letters
 
   drawOn keyboard lines
   sequence_ $ V.map (uncurry drawOn) $ V.zip lines letters
